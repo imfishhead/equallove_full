59ed8a9c25b1ab55d0483b8b6b3411a6a0c638d5
Adele Yu <fhead17@gmail.com>
2017-06-13 01:36:51 +0800

    done adding nested forms and show to admin category

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Category do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :id, :title, :parent_id, :on, sub_categories_attributes: [:id, :title, :parent_id, :on]
 #
 # or
 #
@@ -12,4 +12,36 @@ ActiveAdmin.register Category do
 #   permitted
 # end
 
+	show do
+		h3 "分類"
+	  attributes_table do
+	    row :title
+	    row :on
+    	h4 "次分類"
+	    category.sub_categories.each do |c|
+		    attributes_table do
+  		   	row :title do
+  		   		c.title
+  		   	end
+  		   	row :on do
+  		   		c.on
+  		   	end
+  			end
+	    end
+	  end
+	end
+
+  form do |f|
+    f.inputs "分類" do
+      f.input :title
+      f.input :on
+    end
+    f.inputs do
+      f.has_many :sub_categories do |a|
+        a.input :title
+        a.input :on
+      end
+    end
+    f.actions
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ActiveRecord::Base
+	has_many :posts
+	belongs_to :parent, foreign_key: :parent_id, class_name: "Category"
+	has_many :sub_categories, foreign_key: :parent_id, class_name: "Category"
+  accepts_nested_attributes_for :sub_categories, allow_destroy: true
 end
