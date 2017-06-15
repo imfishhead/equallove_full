d40a5e62f2d2be2fb1fbf872b73e098809a52d45
Adele Yu <fhead17@gmail.com>
2017-06-16 03:09:54 +0800

    add link to each block in index page

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -19,20 +19,20 @@ $(window).load(function() {
 		});
 	});
 
-	$(".s4-faq-more-btn").click(function() {
-		$("body").css("overflow", "hidden");
-		var t = $(this).parent().find('h4').html();
-		var p = $(this).parent().find('p').html();
-		$(".pop-up-box[data-show='5'] h3").html(t)
-		$(".pop-up-box[data-show='5'] p").html(p)
-		$(".pop-up").fadeIn(300);
-		$(".pop-up-box").fadeOut(0);
-		$(".pop-up-box[data-show='5']").fadeIn(0);
-		$(".close-circle").click(function() {
-			$(".pop-up").fadeOut(0);
-			$("body").css("overflow", "auto");
-		});
-	})
+	// $(".s4-faq-more-btn").click(function() {
+	// 	$("body").css("overflow", "hidden");
+	// 	var t = $(this).parent().find('h4').html();
+	// 	var p = $(this).parent().find('p').html();
+	// 	$(".pop-up-box[data-show='5'] h3").html(t)
+	// 	$(".pop-up-box[data-show='5'] p").html(p)
+	// 	$(".pop-up").fadeIn(300);
+	// 	$(".pop-up-box").fadeOut(0);
+	// 	$(".pop-up-box[data-show='5']").fadeIn(0);
+	// 	$(".close-circle").click(function() {
+	// 		$(".pop-up").fadeOut(0);
+	// 		$("body").css("overflow", "auto");
+	// 	});
+	// })
 
 
 	/// 數字
