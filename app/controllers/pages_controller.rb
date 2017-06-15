2775b01e471d73a88b26c5c3406db6c5214d4e7a
Adele Yu <fhead17@gmail.com>
2017-06-16 02:24:14 +0800

    insert front page pinned categories and posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def index
     @pinned_articles = Article.on.pinned
+    @front_page_pinned = Category.on.first(4)
     @categories = Category.on.is_main
     @sub_categories = Category.on.is_subject.where(id: Post.on.pluck(:category_id))
   end
