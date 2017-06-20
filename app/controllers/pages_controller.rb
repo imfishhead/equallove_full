e932974605c51a06f413dd8bc01a9bb6283e5792
Adele Yu <fhead17@gmail.com>
2017-06-20 23:30:35 +0800

    fix pinned posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,14 @@
 class PagesController < ApplicationController
   def index
     @pinned_articles = Article.on.pinned
-    @front_page_pinned_newest = Category.where("title LIKE ?", "%最新%").first
-    @front_page_pinned_news = Category.where("title LIKE ?", "%新聞%").first
-    @front_page_pinned_rumors = Category.where("title LIKE ?", "%闢謠%").first
-    @front_page_pinned_videos = Category.where("title LIKE ?", "%影音%").first
+    @category_newest = Category.where("title LIKE ?", "%最新%").first
+    @pinned_newest = @category_newest.articles.on.front_page_pinned.first
+    @category_news = Category.where("title LIKE ?", "%新聞%").first
+    @pinned_news = @category_news.articles.on.front_page_pinned.first
+    @category_rumor = Category.where("title LIKE ?", "%闢謠%").first
+    @pinned_rumor = @category_rumor.articles.on.front_page_pinned.first
+    @category_video = Category.where("title LIKE ?", "%影音%").first
+    @pinned_video = @category_video.videos.on.front_page_pinned.first
     @categories = Category.on.is_main
     @sub_categories = Category.where(id: Category.on.with_posts.is_subject)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,19 @@ class Post < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :admin_user
 	before_save :only_one_post_pinned_per_category
+	before_save :check_post_should_be_on_if_pinned
 
 	scope :on, -> { where(on: true) }
+	scope :front_page_pinned, -> { where(front_page_pinned: true) }
 	scope :without, -> id { where.not(id: id) if id }
 
 	mount_uploader :main_pic, MainPicUploader
 
 	def only_one_post_pinned_per_category
 		category.posts.without(id).update_all(front_page_pinned: false) if front_page_pinned?
+	end
+
+	def check_post_should_be_on_if_pinned
+		errors.add(:on, "置頂的話此篇必須也要開啟喔！") if front_page_pinned? && !on?
 	end
 end
