9afdfc9309a78daefaddc4bd6af90e2faec60c62
DopplerKuo <doppler.kuo@gmail.com>
2019-05-21 22:08:17 +0800

    add 50% new nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
-  def is_active(id, category)
-  	unless id.nil?
-	    "active" if id.to_i == category.id
-	  end
-  end
+	def is_active(category_name)
+		return unless @category
+		return 'active' if @category.id.to_s == Category.find_by(name: category_name).id.to_s 
+	end
 end
