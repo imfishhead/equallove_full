f5ccafc072e67aef3b09c20dc92a5f85284454dc
DopplerKuo <doppler.kuo@gmail.com>
2019-05-28 17:19:25 +0800

    fix category name to title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def is_active(category_name)
 		return unless @category
-		return 'active' if @category.id.to_s == Category.find_by(name: category_name).id.to_s 
+		return 'active' if @category.id.to_s == Category.find_by(title: category_name).id.to_s 
 	end
 end
